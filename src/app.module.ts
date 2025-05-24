327ec8a1e3c4f9f710727ce52a8c2b82ec43327f
moodilab <jcp@moodilab.kr>
2025-05-24 16:55:11 +0900

    [Feat]_인증, 사용자, 감정일기, 리포트 모듈

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MoodModule } from './mood/mood.module';
+import { ReportModule } from './report/report.module';
 
 @Module({
   imports: [
-        ConfigModule.forRoot({ isGlobal: true })
+        ConfigModule.forRoot({ isGlobal: true }),
+        AuthModule,
+        UserModule,
+        MoodModule,
+        ReportModule
     ],
   controllers: [AppController],
   providers: [AppService],
