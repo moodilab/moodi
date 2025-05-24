c2566f1f15ffb704e4e88137eb10c11f8bc11cc0
moodilab <jcp@moodilab.kr>
2025-05-24 17:54:17 +0900

    [Fix] _ 구문 오류 수정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,16 @@
+// src/user/user.module.ts
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [
+    // ★ 이 줄을 반드시 추가해야 UserRepository가 주입됩니다.
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
 })
 export class UserModule {}
