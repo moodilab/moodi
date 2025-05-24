61d707ac37edfb1fd9f928a8d0eb5b5d2c4464ce
moodilab <jcp@moodilab.kr>
2025-05-24 17:34:15 +0900

    [Feat] _ DB 세팅중

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,16 @@ import { ReportModule } from './report/report.module';
         UserModule,
         MoodModule,
         ReportModule
+        TypeOrmModule.forRootAsync({
+          useFactory: (config: ConfigService) => ({
+            type: 'postgres',
+            url: config.get<string>('DATABASE_URL'),
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,  // 개발 중에만 true. 배포 전에는 false 권장
+          }),
+          inject: [ConfigService],
+        }),
+        
     ],
   controllers: [AppController],
   providers: [AppService],
