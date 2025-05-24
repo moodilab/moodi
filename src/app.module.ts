f4a6f14d268f64edc664ec2f2443d945cfcf25af
moodilab <jcp@moodilab.kr>
2025-05-24 18:31:39 +0900

    [Fix] _ 앱모듈 수정 URL 체크

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
         ConfigModule.forRoot({ isGlobal: true }),
 
-        TypeOrmModule.forRootAsync({
-          useFactory: (config: ConfigService) => ({
-            type: 'postgres',
-            url: config.get<string>('DATABASE_URL'),
-            ssl: { rejectUnauthorized: false },
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,  // 개발 중에만 true. 배포 전에는 false 권장
-          }),
-          inject: [ConfigService],
-        }),
+// src/app.module.ts
+TypeOrmModule.forRootAsync({
+  useFactory: (config: ConfigService) => {
+    const url = config.get<string>('DATABASE_URL');
+    console.log('🔌 DATABASE_URL=', url);
+    return {
+      type: 'postgres',
+      url,
+      ssl: { rejectUnauthorized: false },
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    };
+  },
+  inject: [ConfigService],
+}),
+
 
         AuthModule,
         UserModule,
