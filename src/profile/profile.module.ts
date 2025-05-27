d54593580e0cd9f61f33c9fd9500372969c9ccf6
moodilab <jcp@moodilab.kr>
2025-05-28 00:08:17 +0900

    [Fix] _ profile.module 누락모듈추가

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';       // ← 추가
+import { User } from '../user/user.entity';           // ← User 엔티티 import
+
+
 
 @Module({
+    imports: [
+    TypeOrmModule.forFeature([User]),                // ← 여기에 등록
+  ],
   providers: [ProfileService],
   controllers: [ProfileController]
 })
