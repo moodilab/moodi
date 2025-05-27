02601e1f6da45a9bac6eee4f6fa538555ecf7f1b
moodilab <jcp@moodilab.kr>
2025-05-28 01:11:18 +0900

    [Fix] _ 오류 수정 중

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 // src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
