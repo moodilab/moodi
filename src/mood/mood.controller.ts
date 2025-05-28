1869c09e9e6851dcfaf1e4bc779fcb2299603659
moodilab <jcp@moodilab.kr>
2025-05-28 17:40:26 +0900

    [Fix] _ 생성 삭제

--- src/mood/mood.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('mood')
-export class MoodController {}
--- src/mood/mood.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MoodService } from './mood.service';
-import { MoodController } from './mood.controller';
-
-@Module({
-  providers: [MoodService],
-  controllers: [MoodController]
-})
-export class MoodModule {}
